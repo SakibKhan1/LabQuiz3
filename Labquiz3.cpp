1db45edeb4e49cb611859d31528ff08a5ac0f5dd
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-04-01 19:41:13 -0400

    Create Labquiz3.cpp

---
+++ Labquiz3.cpp
@@ -0,0 +1,28 @@
+//Name: Sakib Khan
+//Prof.Tong Yi 
+//Lab Quiz 3
+
+
+
+#include<iostream>
+using namespace std; 
+
+int main(){
+    string firstNum, secondNum;
+    cout << "Print first number: ";
+    cin >> firstNum;
+    cout << "Print second number: "; 
+    cin >> secondNum;
+    if(firstNum > secondNum){
+        cout << firstNum;
+    }else{
+        cout << secondNum;
+    }
+    
+    
+    
+    
+    
+    
+    return 0;
+}
